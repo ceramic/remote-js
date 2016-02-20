1a56c0dbaaf9af8bff7d522e3973cf893234ee8b
Fernando Borretti <eudoxiahp@gmail.com>
2016-02-20 20:20:10 -0300

    Add a buffered context

--- src/remote-js.lisp
+++ src/remote-js.lisp
@@ -105,8 +105,61 @@ RemoteJS.ws.onopen = function() {
       (:script (cl-markup:raw (js context)))))))
 
 (defgeneric eval (context string)
-  (:documentation "Send some JavaScript to evaluate remotely.")
+  (:documentation "Send some JavaScript to evaluate remotely. Return the
+  string.")
 
   (:method ((context context) string)
     (trivial-ws:send (first (trivial-ws:clients (context-server context)))
-                     string)))
+                     string)
+    string))
+
+;;; Buffered context
+
+(defclass buffered-context ()
+  ((%context :initarg :context)
+   (connected :accessor context-connected-p
+              :initform nil
+              :documentation "Whether or not the client is connected.")
+   (buffer :accessor context-buffer
+           :initarg :buffer
+           :initform nil
+           :type list
+           :documentation "A list of JavaScript strings to evaluate."))
+  (:documentation "The buffered context stores evaluation commands in a buffer
+  until a client connects, then sends all of them at once."))
+
+(defun make-buffered-context (&key (port (find-port:find-port)) (callback +default-callback+))
+  "Create a bufferedcontext object."
+  (let* ((buf-ctx (make-instance 'buffered-context))
+         (ctx (make-instance 'context
+                             :port port
+                             :callback #'(lambda (message)
+                                           (if (string= message +connected-message+)
+                                               (setf (context-connected-p buf-ctx) t)
+                                               (funcall callback message))))))
+    (setf (context-buffer buf-ctx) ctx)
+    buf-ctx))
+
+(defmethod eval ((context buffered-context) string)
+  "Send JavaScript to evaluate, if the buffer is connected. Otherwise, add the code to the buffer.
+
+If there's anything in the buffer and the client is connected, send it all in
+order before evaluating the string."
+  (with-slots (%context connected buffer) context
+    (if connected
+        ;; The client is connected.
+        (progn
+          ;; Do we have a backlog?
+          (when buffer
+            ;; Send it all
+            (mapcar #'(lambda (message)
+                        (eval %context message))
+                    ;; In order!
+                    (reverse buffer))
+            ;; Clean the buffer
+            (setf buffer nil))
+          ;; Evalutate the string
+          (eval %context string))
+        ;; The client is not connected, add this to the buffer
+        (push string buffer)))
+  string)
